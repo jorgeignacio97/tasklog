35ce6166ba47dd913a77cc699409a72a58bb45ba
jorge <jorgelgp37@gmail.com>
2026-07-10 12:57:39 -0400

    chore: initial scaffold from Vite template

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+)
